9235b8f84f631edbef5e0554641d96e207eb8362
Greg Wilson <gvwilson@third-bit.com>
2018-11-14 17:13:41 -0500

    Getting tests to pass

--- R/nickr.R
+++ R/nickr.R
@@ -5,26 +5,49 @@
 #' @name nickr
 NULL
 
-#' Check that a condition holds in a pipe, logging if not.
+#' Check that a condition holds on the columns of data between pipe stages.
 #'
 #' @param .data Incoming data (omitted if in pipe).
-#' @param cond Column-wise condition to test.
-#' @param message User message to display if test fails.
+#' @param ... Column-wise condition expressions to test.
+#' @param msg User message to display if test fails.
 #' @param logger Function to call with message (e.g., `warning` or `stop`).
+#' @param active Is this check turned on (default TRUE). Set FALSE to disable test (e.g., in production).
 #'
 #' @return Input data without modification.
 #'
 #' @export
 
-nickr <- function(.data, cond, message, logger = stop) {
-  cond <- rlang::enquo(cond)
-  if (!all(dplyr::transmute(.data, !!cond)[[1]])) {
-    message <- paste0(message, ': ', as.character(cond))
-    if (is.null(logger)) {
-      stop(message)
-    } else {
-      logger(message)
+nickr_col <- function(.data, ..., msg = "nickr", logger = stop, active = TRUE) {
+
+  # If not active, return immediately.
+  if (!active) {
+    return(invisible(.data))
+  }
+
+  # Check.
+  conditions <- rlang::enquos(...)
+  specifics <- ""
+  for (cond in conditions) {
+    # Check this condition.
+    cond_result <- all(rlang::eval_tidy(cond, .data))
+
+    # Accumulate error messages.
+    if (!cond_result) {
+      this_msg <- deparse(rlang::quo_get_expr(cond))
+      if (specifics == "") {
+        specifics <- this_msg
+      } else {
+        specifics <- paste(specifics, this_msg, sep = ", ")
+      }
     }
   }
+
+  # Report.
+  if (specifics != "") {
+    msg <- paste0(msg, ": ", specifics)
+    logger(msg)
+  }
+
+  # Return data for the next stage in the pipe.
   invisible(.data)
 }
--- tests/testthat/test_nickr.R
+++ tests/testthat/test_nickr.R
@@ -8,51 +8,144 @@ make_fixture <- function() {
   )
 }
 
-test_that("a simple expression works without error", {
+test_that("a correct simple expression works without reporting an error", {
   fixture <- make_fixture()
-  result <- nickr(fixture, a < b, 'should not appear')
+  result <- nickr_col(fixture, a < b, msg = "should not appear")
   expect_equal(fixture, result)
 })
 
-test_that("a simple invalid expression produces an error", {
+test_that("a false simple expression produces an error", {
   fixture <- make_fixture()
-  msg <- 'test failed'
-  expect_error(nickr(fixture, a > b, msg), msg)
+  msg <- "test failed"
+  expect_error(nickr_col(fixture, a > b, msg = msg),
+               regexp = msg)
 })
 
-test_that("a simple invalid expression produces a warning if told to", {
+test_that("a false simple expression produces a warning instead of an error if told to", {
   fixture <- make_fixture()
-  msg <- 'test failed'
-  expect_warning(nickr(fixture, a > b, msg, logger = warning), msg)
+  msg <- "test failed"
+  expect_warning(nickr_col(fixture, a > b, msg = msg, logger = warning),
+                 regexp = msg)
 })
 
-test_that("a complex expression works without error", {
+test_that("a correct multi-part expression works without error", {
   fixture <- make_fixture()
-  result <- nickr(fixture, (a < b) & (b < c), 'should not appear')
+  result <- nickr_col(fixture, (a < b) & (b < c), msg = "should not appear")
   expect_equal(fixture, result)
 })
 
-test_that("a simple expression in a pipe without error", {
+test_that("multiple correct conditions work without error", {
+  fixture <- make_fixture()
+  result <- nickr_col(fixture, a < b, b < c, 0 < c, msg = "should not appear")
+  expect_equal(fixture, result)
+})
+
+test_that("a correct simple expression in a pipe works without error", {
   expected <- make_fixture() %>%
     dplyr::transmute(a)
   result <- make_fixture() %>%
-    nickr(a < b, 'should not appear') %>%
+    nickr_col(a < b, msg = "should not appear") %>%
     dplyr::transmute(a)
   expect_equal(expected, result)
 })
 
-test_that("a simple expression in a pipe with an error", {
-  msg <- 'pipe error'
-  expect_error(make_fixture() %>%
-                 nickr(a > b, msg) %>%
+test_that("a false expression in a pipe produces an error", {
+  msg <- "pipe error"
+  fixture <- make_fixture()
+  expect_error(fixture %>%
+                 nickr_col(a > b, msg = msg) %>%
                  dplyr::transmute(a),
-               msg)
+               regexp = msg)
 })
 
-test_that("a simple expression in a pipe with a warning", {
-  msg <- 'pipe error'
-  expect_warning(make_fixture() %>%
-                   nickr(a > b, msg, logger = warning) %>%
+test_that("a false expression in a pipe produces a warning and the correct result", {
+  msg <- "pipe warning"
+  expected <- make_fixture() %>%
+    dplyr::transmute(a)
+  expect_warning(result <- make_fixture() %>%
+                   nickr_col(a > b, msg = msg, logger = warning) %>%
                    dplyr::transmute(a),
-               msg)
+                 regexp = msg)
+  expect_equal(expected, result)
+})
+
+test_that("all failing conditions show up in the error message in order", {
+  msg <- "pipe error"
+  fixture <- make_fixture()
+  expect_error(fixture %>%
+                 nickr_col(a < 0, b < 0, c < 0, msg = msg) %>%
+                 dplyr::transmute(a),
+               regexp = "pipe error: a < 0, b < 0, c < 0")
+})
+
+test_that("only failing conditions show up in the error message", {
+  msg <- "pipe error"
+  fixture <- make_fixture()
+  expect_error(fixture %>%
+                 nickr_col(0 < b, c < 0, msg = msg) %>%
+                 dplyr::transmute(a),
+               regexp = "pipe error: c < 0")
+})
+
+test_that("default error message shows up if nothing else provided", {
+  fixture <- make_fixture()
+  expect_error(fixture %>% nickr_col(b < 0),
+               regexp = "nickr: b < 0")
+})
+
+test_that("user-defined logging function is not called if nothing goes wrong", {
+  captured_msg <- NULL
+  local_logger <- function(text) {
+    captured_msg <<- text
+  }
+  fixture <- make_fixture()
+  result <- fixture %>% nickr_col(0 < b, logger = local_logger)
+  expect_equal(fixture, result)
+  expect_equal(captured_msg, NULL)
+})
+
+test_that("user-defined logging function is called if something goes wrong", {
+  captured_msg <- NULL
+  local_logger <- function(text) {
+    captured_msg <<- text
+  }
+  fixture <- make_fixture()
+  result <- fixture %>% nickr_col(b < 0, msg = "pipe error", logger = local_logger)
+  expect_equal(fixture, result)
+  expect_equal(captured_msg, "pipe error: b < 0")
+})
+
+test_that("a single inactive test is not run", {
+  fixture <- make_fixture()
+  expected <- make_fixture() %>%
+    dplyr::transmute(a)
+  result <- fixture %>%
+    nickr_col(b < 0, active = FALSE) %>%
+    dplyr::transmute(a)
+  expect_equal(expected, result)
+})
+
+test_that("disabling one test does not disable other tests", {
+  fixture <- make_fixture()
+  expect_error(fixture %>%
+                 nickr_col(b < 0, msg = "check b", active = FALSE) %>%
+                 nickr_col(c < 0, msg = "check c"),
+               regexp = "check c: c < 0")
+})
+
+test_that("execution halts at the first error-level report", {
+  fixture <- make_fixture()
+  expect_error(fixture %>%
+                 nickr_col(b < 0, msg = "check b") %>%
+                 nickr_col(c < 0, msg = "check c"),
+               regexp = "check b: b < 0$")
+})
+
+test_that("multiple warning-level reports appear", {
+  fixture <- make_fixture()
+  expect_warning(fixture %>%
+                   nickr_col(b < 0, msg = "check b", logger = warning) %>%
+                   nickr_col(c < 0, msg = "check c", logger = warning),
+                 regexp = "(check b: b < 0)|(check c: c < 0)",
+                 all = TRUE)
 })
